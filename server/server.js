c02bff94703bd948aeaf483cd66e558cfc7d320c
sssaikyosan <saikyosan.agar@gmail.com>
2025-06-14 09:21:41 +0900

    データファイル移動

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ import { getDisplayRating, calRating } from './utils.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const RATING_FILE = path.join(__dirname, 'ratings.json');
+const RATING_FILE = path.join(__dirname, '../Data/ratings.json');
 
 
 const app = express();
